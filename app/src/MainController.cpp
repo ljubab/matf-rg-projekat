a285250ca3226c59ac6fc446d26369aa9fd631ac
Ljubomir Banovic <ljuba.banovic@gmail.com>
2026-07-19 00:01:29 +0200

    Reposition Shiba.

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -80,7 +80,7 @@ namespace app {
         shader->set_mat4("view", graphics->camera()->view_matrix());
 
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(-3.0, 0.0, -3.0));
+        model = glm::translate(model, glm::vec3(-1.5, 0.31, -1.0));
         model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
         model = glm::rotate(model, platform->frame_time().current, glm::vec3(0.0f, 0.0f, 1.0f));
         model = glm::scale(model, glm::vec3(0.3f));
