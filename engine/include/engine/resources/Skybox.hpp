3d7566e910579dee3582b1bdc5c237bb97852913
Ljubomir Banovic <ljuba.banovic@gmail.com>
2026-08-13 00:59:54 +0200

    fix: skybox documentation

--- engine/include/engine/resources/Skybox.hpp
+++ engine/include/engine/resources/Skybox.hpp
@@ -19,23 +19,23 @@ class Skybox {
 
 public:
     /**
-    * @brief Returns the OpenGL ID of the mountain_skybox.
-    * @returns The OpenGL ID of the mountain_skybox.
+    * @brief Returns the OpenGL ID of the skybox.
+    * @returns The OpenGL ID of the skybox.
     */
     uint32_t vao() const {
         return m_vao;
     }
 
     /**
-    * @brief Returns the OpenGL ID of the mountain_skybox texture.
-    * @returns The OpenGL ID of the mountain_skybox texture.
+    * @brief Returns the OpenGL ID of the skybox texture.
+    * @returns The OpenGL ID of the skybox texture.
     */
     uint32_t texture() const {
         return m_texture_id;
     }
 
     /**
-    * @brief Destroys the mountain_skybox object in the OpenGL context.
+    * @brief Destroys the skybox object in the OpenGL context.
     */
     void destroy();
 
@@ -49,10 +49,10 @@ private:
 
     /**
     * @brief Constructs a Skybox object.
-    * @param vao The OpenGL ID of the mountain_skybox.
-    * @param texture_id The OpenGL ID of the mountain_skybox texture.
-    * @param path The path to the mountain_skybox texture.
-    * @param name The name of the mountain_skybox.
+    * @param vao The OpenGL ID of the skybox.
+    * @param texture_id The OpenGL ID of the skybox texture.
+    * @param path The path to the skybox texture.
+    * @param name The name of the skybox.
     */
     Skybox(uint32_t vao, uint32_t texture_id, std::filesystem::path path, std::string name)
         : m_vao(vao)
